fd7475c31ec2d539ea7ad9af819f6dc82daba80f
Rossicler Júnior <rossiclerjr@gmail.com>
2021-07-12 15:05:41 -0300

    nlwValoriza project with creation of users and tags using typeorm, sqlite and express

---
+++ nlwValoriza/src/routes.ts
@@ -0,0 +1,11 @@
+import { Router } from "express";
+import { CreateUserController } from "./controllers/CreateUserController";
+import { CreateTagController } from "./controllers/CreateTagController";
+
+export const router = Router();
+
+const createUserController = new CreateUserController();
+const createTagController = new CreateTagController();
+
+router.post("/users", createUserController.handle);
+router.post("/tags", createTagController.handle);
